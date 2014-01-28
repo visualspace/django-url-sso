a31299720b7c37f84925db4b23a1dd6892063fb8
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-01-28 17:41:18 +0100

    Preliminary API for Intershift plugin.

--- url_sso/plugins/intershift.py
+++ url_sso/plugins/intershift.py
@@ -19,18 +19,132 @@
 
 """ SSO for Intershift http://www.intershift.nl/ """
 
-from .utils import Singleton
+import requests
+import urllib
+
+from lxml import etree
+
+from ..utils import Singleton
+from ..settings import url_sso_settings
+
+intershift_settings = url_sso_settings.INTERSHIFT
 
 
 class IntershiftPlugin(object):
     __metaclass__ = Singleton
 
-    bogus_dict = {
-        'MY_URL': 'https://www.bogus.com/some_token'
-    }
+    def _get_site_url(self, site_name):
+        """ Util method to get site url from name. """
+
+        assert 'sites' in intershift_settings
+        assert site_name in intershift_settings['sites']
+        assert 'url' in intershift_settings['sites'][site_name]
+
+        return intershift_settings['sites'][site_name]['url']
+
+    def _parse_login_key(self, data):
+        """ Parse returned (broken) XML and return loginkey. """
+
+        # Use parse with recovery enabled
+        parser = etree.XMLParser(recover=True)
+
+        root = etree.fromstring(data, parser)
+
+        key_element = root.find('key')
+        value = key_element.get('value')
+
+        if not value:
+            raise Exception('No value found in login key response.')
+
+        return value
+
+    def _request_login_key(self, site_name, username):
+        """ Request and return login URL for a particular site and user. """
+
+        assert 'secret' in intershift_settings
+
+        # Send out request
+        r = requests(
+            self._get_site_url(site_name),
+            params={
+                'user': username,
+                'secret': intershift_settings['secret']
+            },
+            verify=True
+        )
+
+        if not r.status_code == 200:
+            raise Exception(
+                'Request returned an error status.'
+            )
+
+        if not r.content:
+            raise Exception(
+                'Login key request returned no content.'
+            )
+
+        return self._parse_login_key(r.content)
+
+    def _generate_login_url(self, site_name, username):
+        """ Generate and return a login URL from site_name and username. """
+
+        # Get site URL
+        site_url = self._get_site_url(site_name)
+
+        # Fetch a login key
+        login_key = self._request_login_key(site_name, username)
+
+        params = urllib.urlencode({
+            'user': username,
+            'login_key': login_key
+        })
+
+        return '{url}?{params}'.format(url=site_url, params=params)
+
+    def _get_login_url_key(self, site_name):
+        """ Utility method returning context key for login URL. """
+
+        assert site_name
+
+        return 'INTERSHIFT_{site}_SSO_URL'.format(
+            site=site_name.upper()
+        )
+
+    def get_login_url(self, site_name, request):
+        """ Return login URL for a particular configured site and user. """
+
+        assert request.user.is_authenticated(), 'User not authenticated.'
+        username = request.user.username
+
+        return self._generate_login_url(site_name, username)
 
     def get_login_urls(self, request):
-        return self.bogus_dict
+        """ Return login URLs  for all configured sites. """
+
+        login_urls = {}
+
+        # Only perform for logged in users
+        if request.user.is_authenticated():
+            for site_name, site in intershift_settings['sites'].iteritems():
+
+                # Determine whether user has access rights
+                has_access = site.get('has_access', None)
+                if has_access and not has_access():
+                    # has_access(request) is defined and user does not have
+                    # access to this site - skip.
+                    continue
+
+                # Get login URL for site and request (user)
+                login_url = self.get_login_url(site_name, request)
+
+                # Generate name for URL in context
+                login_url_key = self._get_login_url_key(site_name)
+
+                # Add key and URL to login_urls dictionary
+                assert not login_url_key in login_urls, 'Duplicate URL.'
+                login_urls[login_url_key] = login_url
+
+        return login_urls
 
 # Instantiate singleton
 intershift_plugin = IntershiftPlugin()
